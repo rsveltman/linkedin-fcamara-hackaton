89f4324d526698d30dba7bd0d0c71aed1cbea92c
Gustavo Calixto de Souza <gustavo.calixtosouza@gmail.com>
2018-03-10 19:24:26 -0300

    v atualizada
    v.01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ListaCandidatosPage } from '../pages/lista-candidatos/lista-candidatos';
+import { CandidatoPage } from '../pages/candidato/candidato';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ListaCandidatosPage,
+    CandidatoPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +23,9 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ListaCandidatosPage,
+    CandidatoPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {ListaCandidatosPage} from '../lista-candidatos/lista-candidatos';
 
 @Component({
   selector: 'page-home',
@@ -8,7 +9,13 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+    
+  }
 
+  nextpage(){
+    this.navCtrl.push(ListaCandidatosPage);
+    console.log(ListaCandidatosPage);
+    
   }
 
 }
